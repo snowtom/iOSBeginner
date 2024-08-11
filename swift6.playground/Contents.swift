3ac02d47730fbe8764f8d32da9fd8143955a5959
tom <tom11.tang@icloud.com>
2024-08-11 22:40:21 +0800

    learn at 2024/8/11

--- swift6.playground/Contents.swift
+++ swift6.playground/Contents.swift
@@ -6,3 +6,13 @@ let pi = 3.1415926
 let greeting = "Hello, \(name)!"
 let circleArea = "The area of a circle with radius \(radius) is \(pi * radius * radius)"
 print(greeting, circleArea)
+
+let quotation = """
+    Even though there's whitespace to the left,
+    the actual lines aren't indented.
+        Except for this line.
+    Double quotes (") can appear without escaped.
+    I still have \(radius * pi) big circle.
+    """
+
+print(quotation)
